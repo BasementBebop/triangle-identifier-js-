e001779e055a3b22bf57e8c1141737f3a40b6387
Sean <basementbebop@gmail.com>
2015-02-13 10:34:29 -0800

    Add html content and jquery script

--- js/triangle-scripts.js
+++ js/triangle-scripts.js
@@ -1,14 +1,27 @@
-var triangle = function(angles) {
-  angles.sort();
-
-  if(angles[2] > angles[0] + angles[1]) {
+var triangle = function(sides) {
+  sides.sort();
+  if(sides[2] > sides[0] + sides[1]) {
     return "not a";
-  } else if(angles[0] === angles[1] && angles[1] === angles[2]) {
+  } else if(sides[0] === sides[1] && sides[1] === sides[2]) {
     return "an equilateral";
-  } else if(angles[0] === angles[1] || angles[1] === angles[2]) {
+  } else if(sides[0] === sides[1] || sides[1] === sides[2]) {
     return "an isosceles";
   } else {
     return "a scalene";
   }
-
 }
+
+$(document).ready(function() {
+  $("form#sides").submit(function(event){
+    var side1 = $("sides#side1").val();
+    var side2 = $("sides#side2").val();
+    var side3 = $("sides#side3").val();
+    var sides = [side1, side2, side3];
+    var output = triangle(sides);
+    $("#output").text(output);
+
+    $(".result").show();
+
+    event.preventDefault();
+  });
+});
